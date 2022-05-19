b5bd4c27ae3ba000415163ea2f4f15e611a1f47b
peteredwards <p.l.edwards@leeds.ac.uk>
2022-05-19 11:17:37 +0100

    incorporated changes made on pilot branch

--- _includes/javascript/components/modal.js
+++ _includes/javascript/components/modal.js
@@ -20,10 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.hash = dialog.originalpagehash
         });
     });
+    setTimeout( () => {
+        openAlertDialog('Spacefinder feedback', 'Can you please help us improve this site by <a id="fb-link" href="https://forms.office.com/Pages/ResponsePage.aspx?id=qO3qvR3IzkWGPlIypTW3y1HKKRLEEutMhj3gY5HWuwpUOFk0R0RQUjEzN0xUTjRXV0pTT1NGTE9HMy4u" target="sf_feedback">providing feedback using this short form</a> (opens in a new browser window / tab).');
+        document.getElementById('fb-link').addEventListener('click', e => {
+            let dialog = new A11yDialog( document.getElementById( 'sfalertdialog' ) );
+            dialog.hide();
+        });
+    }, 30000)
+    
 });
 function openAlertDialog( title, content ) {
     document.getElementById('sfalertdialog-title').textContent = title;
-    document.getElementById('sfalertdialog-description').textContent = content;
+    document.getElementById('sfalertdialog-description').innerHTML = content;
     let dialog = new A11yDialog( document.getElementById( 'sfalertdialog' ) );
     dialog.show();
     dialog.on('hide', (element, event) => {
