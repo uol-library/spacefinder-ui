25c46d6b9e4ad7960f102753d219a4e48cc34c61
peteredwards <p.l.edwards@leeds.ac.uk>
2022-05-25 16:00:43 +0100

    Added visibility toggle on filters pane for keyboard navigation. Corrected license.

--- _includes/javascript/spaces.js
+++ _includes/javascript/spaces.js
@@ -67,8 +67,8 @@ function applyFilters() {
         document.querySelectorAll('.list-space').forEach( el => {
             el.classList.remove('hidden');
         });
-        deselectSpaces();
     }
+    deselectSpaces();
     document.dispatchEvent( new Event( 'filtersapplied' ) );
 }
 
@@ -208,7 +208,9 @@ function selectSpace( spaceid, source ) {
 
 /**
  * Deselects a space in the list, an optionally scrolls the list to the top
+ * and recentres the map.
  * @param {boolean} scrollReset 
+ * @param {boolean} mapReset 
  */
 function deselectSpaces( scrollReset ) {
     if ( spacefinder.infoWindow ) {
@@ -223,6 +225,10 @@ function deselectSpaces( scrollReset ) {
     if ( scrollReset ) {
         document.getElementById('listcontainer').scrollTop = 0;
     }
+    if ( mapReset ) {
+        document.dispatchEvent( 'resetmap' );
+    }
+
 }
 
 /**
