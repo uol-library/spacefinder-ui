82be5353d73abf2767bac954b7170c6c37c30288
peteredwards <p.l.edwards@leeds.ac.uk>
2022-05-26 19:21:08 +0100

    fixed js bug

--- _includes/javascript/spaces.js
+++ _includes/javascript/spaces.js
@@ -210,7 +210,6 @@ function selectSpace( spaceid, source ) {
  * Deselects a space in the list, an optionally scrolls the list to the top
  * and recentres the map.
  * @param {boolean} scrollReset 
- * @param {boolean} mapReset 
  */
 function deselectSpaces( scrollReset ) {
     if ( spacefinder.infoWindow ) {
@@ -225,10 +224,6 @@ function deselectSpaces( scrollReset ) {
     if ( scrollReset ) {
         document.getElementById('listcontainer').scrollTop = 0;
     }
-    if ( mapReset ) {
-        document.dispatchEvent( 'resetmap' );
-    }
-
 }
 
 /**
